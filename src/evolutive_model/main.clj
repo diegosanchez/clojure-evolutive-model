856b8d47ad3ae33894e2a9175f2ea7217bfc0ac9
Diego Sanchez <diego.sanches@gmail.com>
2020-08-24 17:26:13 -0300

    Month validation

--- src/evolutive_model/main.clj
+++ src/evolutive_model/main.clj
@@ -1,5 +1,13 @@
 (ns evolutive-model.main
+  (:require [clojure.spec.alpha :as s])
   (:gen-class))
+
+(s/def ::mes
+  (s/and pos-int? (s/int-in 1 13)))
+
+(defn feriado-puntual [dia mes anio]
+  {:pre [(s/valid? ::mes mes)]}
+  {:dia dia :mes mes :anio anio})
 
 (defn es-feriado?
   [fecha-consulta feriados]
--- test/evolutive_model/main_test.clj
+++ test/evolutive_model/main_test.clj
@@ -7,12 +7,16 @@
     (is (= 0 0))))
 
 (def dic-10-2020
-  {:dia 10 :mes 12 :anio 2020})
+  (feriado-puntual 10 12 2020))
 
 (def hoy
   {:dia 24 :mes 8 :anio 2020})
 
-(deftest feriado-puntual
+(deftest test-feriado-puntual
   (testing "10-12-2020 es feriado puntual"
     (is (not (es-feriado? hoy [{:dia 10 :mes 12 :anio 2020}])))
     (is (es-feriado? dic-10-2020 [{:dia 10 :mes 12 :anio 2020}]))))
+
+(deftest test-feriado-puntual-creacion
+  (testing "feriadob puntual invalido"
+   (is (thrown? AssertionError (feriado-puntual 1 13 2020)))))
