e0e585bc1a8fae309a61ff7b94f04585f0a848c8
Diego Sanchez <diego.sanchez@etermax.com>
2020-08-24 09:33:18 -0300

    Trivial code

--- src/evolutive_model/main.clj
+++ src/evolutive_model/main.clj
@@ -1,5 +1,9 @@
-te(ns evolutive-model.main
+(ns evolutive-model.main
   (:gen-class))
+
+(defn es-feriado?
+  [fecha-consulta feriados]
+  true)
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/evolutive_model/main_test.clj
+++ test/evolutive_model/main_test.clj
@@ -5,3 +5,10 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 0))))
+
+(def dic-10-2020
+  {:dia 10 :mes 12 :anio 2020})
+
+(deftest feriado-puntual
+  (testing "10-12-2020 es feriado puntual"
+    (is (es-feriado? dic-10-2020 [{:dia 10 :mes 12 :anio 2020}]))))
