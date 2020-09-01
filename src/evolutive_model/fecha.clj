9a92932acf04cab08b2eda307e0388fd3aabcc8a
Diego Sanchez <diego.sanches@gmail.com>
2020-09-01 08:47:06 -0300

    Namespaces keys #2

--- src/evolutive_model/fecha.clj
+++ src/evolutive_model/fecha.clj
@@ -9,16 +9,16 @@
 (s/def ::mes
   (s/and pos-int? (s/int-in 1 13)))
 
-(s/def ::fecha
+(s/def ::date
   (s/keys :req [::dia pos-int?
-               ::mes ::mes
-               ::anio pos-int?]))
+                ::mes ::mes
+                ::anio pos-int?]))
 
 (s/fdef fecha
   :args (s/cat ::dia pos-int?
                ::mes ::mes
                ::anio pos-int?)
-  :ret ::fecha)
+  :ret ::date)
 
 (defn fecha [dia mes anio]
   {::dia dia ::mes mes ::anio anio})
