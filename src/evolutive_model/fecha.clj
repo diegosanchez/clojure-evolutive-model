74729eebb25d0848d80481a52434980525a1a3b4
Diego Sanchez <diego.sanches@gmail.com>
2020-10-04 13:31:56 -0300

    fecha generator created

--- src/evolutive_model/fecha.clj
+++ src/evolutive_model/fecha.clj
@@ -5,6 +5,9 @@
 (s/def ::mes
   (s/int-in 1 13))
 
+(s/def ::anio
+  (s/int-in 1900 2400))
+
 (defn mes-con-31-dias?
   [mes]
   (not (nil? (some #(= mes %) '(1 3 5 7 10 12)))))
@@ -25,6 +28,7 @@
 
 (defn fecha [anio mes dia]
   {:pre [(and
+          (s/valid? ::anio anio)
           (s/valid? ::mes mes)
           (or
            (and (mes-con-31-dias? mes)         (s/valid? (mes-con-maximo-de-dias 31) dia))
@@ -33,17 +37,33 @@
            (and (anio-bisiesto? anio)          (s/valid? (mes-con-maximo-de-dias 29) dia))))]}
   {:dia dia :mes mes :anio anio})
 
+(defn mayor [fecha1 fecha2]
+  (> (fecha-2-timestamp fecha1) (fecha-2-timestamp fecha2)))
+
+(defn mismo-dia [fecha numero-dia]
+  (=
+   numero-dia
+   (.getDay (fecha-2-java fecha))))
 
 
-
-
-
-
-
-
-
-
-
+(def gen-anio-mes-dia
+  (gen/bind
+    (gen/tuple
+     (s/gen ::anio)
+     (s/gen ::mes))
+    (fn [lst]
+      (let [[anio mes] lst]
+        (gen/tuple
+         (gen/return anio)
+         (gen/return mes)
+         (cond
+           (mes-con-30-dias? mes)       (s/gen (mes-con-maximo-de-dias 30))
+           (mes-con-31-dias? mes)       (s/gen (mes-con-maximo-de-dias 31))
+           (anio-bisiesto? anio)        (s/gen (mes-con-maximo-de-dias 29))
+           (not (anio-bisiesto? anio))  (s/gen (mes-con-maximo-de-dias 28))))))))
+          
+(def gen-fecha
+  (gen/fmap #(apply fecha %) gen-anio-mes-dia))
 
 
 (defn fecha-2-java [fecha]
@@ -57,48 +77,4 @@
                  (- (::anio fecha) 1900)
                  (- (::mes fecha) 1)
                  (::dia fecha))))
-
-(defn mayor [fecha1 fecha2]
-  (> (fecha-2-timestamp fecha1) (fecha-2-timestamp fecha2)))
-
-(defn mismo-dia [fecha numero-dia]
-  (=
-   numero-dia
-   (.getDay (fecha-2-java fecha))))
-
-(defn leap-year-return-or
-  [year value-leap-year value-not-leap-year]
-  (cond (zero? (mod year 400)) value-leap-year
-        (zero? (mod year 100)) value-not-leap-year
-        (zero? (mod year 4)) value-leap-year
-        :default value-not-leap-year))
-
-(defn month-biggest-day
-  [month year]
-  (cond
-    (= month 2) (leap-year-return-or year 29 28)
-    (some #(= month %) '(1 3 5 7 9 10 12)) 31
-    :else 30))
-
-(def gen-month-and-year
-  (gen/tuple
-   (s/gen (s/and pos-int? (s/int-in 1 13)))
-   (s/gen (s/and pos-int? (s/int-in 1 500)))))
-
-(defn gen-days-smaller-than 
-  [max]
-  (s/gen (s/and pos-int? (s/int-in 1 (+ max 1)))))
-
-(def gen-day-and-month
-  (gen/bind
-   gen-month-and-year
-   (fn [month-and-year]
-     (gen/tuple
-      (gen/return (last month-and-year))
-      (gen/return (first month-and-year))
-      (gen-days-smaller-than 
-       (apply month-biggest-day month-and-year))))))
-
-(def generador-fecha
-  (gen/fmap #(apply fecha %) gen-day-and-month))
 
