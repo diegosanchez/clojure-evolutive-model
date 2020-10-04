bcaadbb0fd6fd169ccc8c5ebaab2e302aac0ffe5
Diego Sanchez <diego.sanches@gmail.com>
2020-10-04 13:53:57 -0300

    [test] fecha generator test added #1

--- src/evolutive_model/fecha.clj
+++ src/evolutive_model/fecha.clj
@@ -2,12 +2,6 @@
   (:require [clojure.spec.gen.alpha :as gen]
             [clojure.spec.alpha :as s]))
 
-(s/def ::mes
-  (s/int-in 1 13))
-
-(s/def ::anio
-  (s/int-in 1900 2400))
-
 (defn mes-con-31-dias?
   [mes]
   (not (nil? (some #(= mes %) '(1 3 5 7 10 12)))))
@@ -26,27 +20,37 @@
   [maximo]
   (s/int-in 1 (+ maximo 1)))
 
+(s/def ::mes-spec
+  (s/int-in 1 13))
+
+(s/def ::anio-spec
+  (s/int-in 1900 2400))
+
+(defn dia-spec
+  [anio mes]
+  (cond
+    (mes-con-30-dias? mes)       (mes-con-maximo-de-dias 30)
+    (mes-con-31-dias? mes)       (mes-con-maximo-de-dias 31)
+    (anio-bisiesto? anio)        (mes-con-maximo-de-dias 29)
+    (not (anio-bisiesto? anio))  (mes-con-maximo-de-dias 28)))
+
 (defn fecha [anio mes dia]
   {:pre [(and
-          (s/valid? ::anio anio)
-          (s/valid? ::mes mes)
-          (or
-           (and (mes-con-31-dias? mes)         (s/valid? (mes-con-maximo-de-dias 31) dia))
-           (and (mes-con-30-dias? mes)         (s/valid? (mes-con-maximo-de-dias 30) dia))
-           (and (comp not anio-bisiesto? anio) (s/valid? (mes-con-maximo-de-dias 28) dia))
-           (and (anio-bisiesto? anio)          (s/valid? (mes-con-maximo-de-dias 29) dia))))]}
+          (s/valid? ::anio-spec anio)
+          (s/valid? ::mes-spec mes)
+          (s/valid? (dia-spec anio mes) dia))]}
   {:dia dia :mes mes :anio anio})
 
 (defn fecha-2-java [fecha]
   (new java.util.Date
-       (- (::anio fecha) 1900)
-       (- (::mes fecha) 1)
+       (- (::anio-spec fecha) 1900)
+       (- (::mes-spec fecha) 1)
        (::dia fecha)))
 
 (defn fecha-2-timestamp [fecha]
   (.getTime (new java.util.Date
-                 (- (::anio fecha) 1900)
-                 (- (::mes fecha) 1)
+                 (- (::anio-spec fecha) 1900)
+                 (- (::mes-spec fecha) 1)
                  (::dia fecha))))
 
 (defn mayor [fecha1 fecha2]
@@ -57,22 +61,18 @@
    numero-dia
    (.getDay (fecha-2-java fecha))))
 
-
 (def gen-anio-mes-dia
   (gen/bind
     (gen/tuple
-     (s/gen ::anio)
-     (s/gen ::mes))
+     (s/gen ::anio-spec)
+     (s/gen ::mes-spec))
     (fn [lst]
       (let [[anio mes] lst]
         (gen/tuple
          (gen/return anio)
          (gen/return mes)
-         (cond
-           (mes-con-30-dias? mes)       (s/gen (mes-con-maximo-de-dias 30))
-           (mes-con-31-dias? mes)       (s/gen (mes-con-maximo-de-dias 31))
-           (anio-bisiesto? anio)        (s/gen (mes-con-maximo-de-dias 29))
-           (not (anio-bisiesto? anio))  (s/gen (mes-con-maximo-de-dias 28))))))))
+         (s/gen (dia-spec anio mes)))))))
+
           
 (def gen-fecha
   (gen/fmap #(apply fecha %) gen-anio-mes-dia))
